6065001952d57d25d8d42b7016ee154c5071f886
Tikumporn Wankvar <tikumporn@Macintosh.local>
2017-10-22 14:49:24 +0700

    Fixed admin botton

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,6 @@
 class SubjectsController < ApplicationController
   before_action :set_subject, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :authenticate_user!, only: [:index, :show, ]
   # GET /subjects
   # GET /subjects.json
   def index
