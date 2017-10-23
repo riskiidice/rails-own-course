2f01728fc5337c2c69b4958a2e3506480b806acb
Tikumporn Wankvar <tikumporn@Macintosh.local>
2017-10-23 21:54:08 +0700

    wait for fix cart javascript

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,5 +1,8 @@
-var init_cart ;
-
+var init_cart;
+var items = [
+  {"subject_id" : "1", "value" : 1},
+  {"subject_id" : "2", "value" : 2},
+];
 $(document).ready(function() {
   var item = $('.cart-item').text();
   if(!item || item == '0'){
@@ -10,17 +13,38 @@ $(document).ready(function() {
 });
 init_cart = function() {
   var cart = localStorage.getItem("cart-id");
+  var items = localStorage.getItem("items");
   if(!cart){
     $.ajax({ url: '/addcart', success: function(result){
       localStorage.setItem("cart-id",result.cart_id);
     }});
   }
+  if(items){
+
+  }
 
 }
+update_cart = function(subjec_id){
+  var items = localStorage.getItem("items");
+  var flag = 0;
+  if(items){
+      for(var i=0;i<items.length;i++){
+        if(items[i].subject_id == subject_id){
+            items[i].value+=1;
+            flag = 1;
+            console.log(items[i]);
+         }
+      }
+  }else{
+    var items = [];
+    localStorage.setItem("items",);
+  }
+  // item = parseInt(item) + 1;
+}
+
 add_cart = function(){
     $('#cart-lookup-form').on('ajax:success', function(event, data, status){
-      var item = $('.cart-item').text();
-      item = parseInt(item) + 1;
+      item = update_cart($('#subject_id').val());
       var data = event.detail[0];
       if(data){
         $('.cart-item').text(item);
