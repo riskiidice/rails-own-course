962d0925830c922167ceb27bf4a33c4766c9c6e0
Tikumporn Wankvar <tikumporn@Macintosh.local>
2017-10-26 21:08:45 +0700

    fied migrate

--- db/migrate/20171021204641_change_references_from_subjects.rb
+++ db/migrate/20171021204641_change_references_from_subjects.rb
@@ -1,6 +1,5 @@
 class ChangeReferencesFromSubjects < ActiveRecord::Migration[5.1]
   def change
-    remove_column :subjects, :references
     add_column :subjects, :students, :references
   end
 end
